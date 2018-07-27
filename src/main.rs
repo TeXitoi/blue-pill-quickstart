2705f57bfec8d6953b69dedb2b5063cb3e61f773
Guillaume Pinot <texitoi@texitoi.eu>
2018-07-27 23:28:12 +0200

    Complete blinking blue pill

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+#![no_main]
+#![no_std]
+
+extern crate cortex_m;
+#[macro_use]
+extern crate cortex_m_rt as rt;
+extern crate panic_semihosting;
+extern crate stm32f103xx_hal as hal;
+
+use rt::ExceptionFrame;
+use hal::prelude::*;
+
+entry!(main);
+
+fn main() -> ! {
+    let dp = hal::stm32f103xx::Peripherals::take().unwrap();
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let mut flash = dp.FLASH.constrain();
+    let mut rcc = dp.RCC.constrain();
+    let mut gpioc = dp.GPIOC.split(&mut rcc.apb2);
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+    let mut delay = hal::delay::Delay::new(cp.SYST, clocks);
+    let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
+
+    loop {
+        led.set_high();
+        delay.delay_ms(1_000u16);
+        led.set_low();
+        delay.delay_ms(1_000u16);
+    }
+}
+
+exception!(HardFault, hard_fault);
+
+fn hard_fault(ef: &ExceptionFrame) -> ! {
+    panic!("{:#?}", ef);
+}
+
+exception!(*, default_handler);
+
+fn default_handler(irqn: i16) {
+    panic!("Unhandled exception (IRQn = {})", irqn);
+}
